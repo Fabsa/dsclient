d66f158c1cd86ad727cf50fdb6337265ac4a3ec4
Fabio Ferreira <103062036+Kinadourado@users.noreply.github.com>
2022-08-01 00:05:00 -0300

    Create Project

---
+++ backend/src/main/java/com/devsystempro/dsclient/DsclientApplication.java
@@ -0,0 +1,13 @@
+package com.devsystempro.dsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsclientApplication.class, args);
+	}
+
+}
