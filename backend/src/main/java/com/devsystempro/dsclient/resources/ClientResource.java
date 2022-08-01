4c5c418f9eff4f6f4506f13eed4600efcfe11ace
Fabio Ferreira <103062036+Kinadourado@users.noreply.github.com>
2022-08-01 20:25:23 -0300

    Erro no ClientResource

---
+++ backend/src/main/java/com/devsystempro/dsclient/resources/ClientResource.java
@@ -0,0 +1,24 @@
+package com.devsystempro.dsclient.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsystempro.dsclient.entities.Client;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {//implementação do controlador REST onde as APIs serao implementadas atraves do controlador rest
+
+
+	@GetMapping	
+	public ResponseEntity<List<Client>>findAll(){
+	List<Client>list = new ArrayList<>();
+	list.add(new Client(1L,"Maria Silvaaa","12345678901",6500.0,"1994-07-20T10:30:00Z",2));
+     return ResponseEntity.ok().body(list);
+   }
+}
